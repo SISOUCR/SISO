26036296b2f6fc90da58d928af6b7c70247aadbb
mlndz <fabimelndez@hotmail.com>
2014-12-11 15:31:51 -0600

    Requests

---
+++ app/src/main/java/com/example/mlndz/siso/Evento.java
@@ -0,0 +1,16 @@
+package com.example.mlndz.siso;
+
+/**
+ * Created by mlndz on 12/11/14.
+ */
+public class Evento {
+
+    private String id;
+    private String title;
+    private String description;
+    private String location;
+    private String start;
+    private String end;
+
+
+}
